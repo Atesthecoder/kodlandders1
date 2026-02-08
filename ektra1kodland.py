649dc641c0e6618c9928385ff3a170cb45778ee4
Atesthecoder <ates28cagan@gmail.com>
2026-02-08 15:15:00 +0300

    wwsw

--- ektra1kodland.py
+++ ektra1kodland.py
@@ -1,11 +1,17 @@
 import random
-print("'2*win with *isim yok*' mekanına hoşgeldiniz!")
-isimce = input("ismin???")
-if len(isimce) < 3:
-    print("isminiz çok kısa, lütfen daha uzun bir isim giriniz.")
+from colorama import Fore, Style
+import sys
+while True:
+    print("'2*win with *isim yok*' mekanına hoşgeldiniz!")
     isimce = input("ismin???")
-if len(isimce) > 10:
-    isimce = isimce[:10] + "..."
-any_strings_of_list = ["Mr.Ates","Atesbey","**++py++**",isimce]
-xrt = random.choice(any_strings_of_list)
-print(xrt, "çıktı kazandın" if xrt == isimce else "çıktı kaybettin" )
+    if len(isimce) < 3:
+        print(Fore.RED+"isminiz çok kısa, lütfen daha uzun bir isim giriniz.")
+        isimce = input("ismin???")
+    if len(isimce) > 10:
+        isimce = isimce[:10] + "..."
+    any_strings_of_list = ["Mr.Ates","Atesbey","**++py++**",isimce]
+    xrt = random.choice(any_strings_of_list)
+    print(xrt, Style.BRIGHT + Fore.LIGHTGREEN_EX +"çıktı kazandın" if xrt == isimce else Style.BRIGHT + Fore.MAGENTA + "çıktı kaybettin" + Style.RESET_ALL)
+    if input("Tekrar oynamak ister misin? (E/H)").upper() != "E":
+        print("Oyun bitti, teşekkürler!")
+        sys.exit()
