37b2159b8ae6753d924a2c6a35f51ff425cb358b
Atesthecoder <ates28cagan@gmail.com>
2026-02-08 18:28:23 +0300

    swqzw

--- Atesbey.py
+++ Atesbey.py
@@ -1,2 +1,47 @@
+import os
+import tkinter
 
-input("Press Enter to continue...")
+tke = tkinter.Tk()
+tke.title("PYTHON MODULLERİ")
+
+
+tke.geometry(f"{400}x{200}")
+button1 = tkinter.Button(tke, text=("dowland pygame!"),background="yellow", command=lambda: os.system("pip install pygame"))
+button2 = tkinter.Button(tke, text=("dowland colorama!"),background="yellow", command=lambda: os.system("pip install colorama"))
+button3 = tkinter.Button(tke, text=("dowland pygamezero!"),background="yellow", command=lambda: os.system("pip install pgzero"))
+button5 = tkinter.Button(tke, text=("dowland pillow!"),background="yellow", command=lambda: os.system("pip install pillow"))
+button4 = tkinter.Button(tke, text=("dowland rich!"),background="yellow", command=lambda: os.system("pip install rich"))
+button4.pack()
+button5.pack()
+button2.pack()
+button3.pack()
+button1.pack()
+
+try:
+    import pygame
+    button1.config(text="pygame yüklü!",background="green")
+except ImportError:
+    print("pygame yüklencek!")
+try:
+    import colorama
+    button2.config(text="colorama yüklü!",background="green")
+except ImportError:
+    print("colorama yüklencek!")
+try:
+    import pygame
+    button3.config(text="pgzero yüklü!",background="green")
+except ImportError:
+    print("pgzero yüklencek!")
+try:
+    import pygame
+    button4.config(text="rich yüklü!",background="green")
+except ImportError:
+    print("rich yüklencek!")
+try:
+    import pygame
+    button5.config(text="pillow yüklü!",background="green")
+except ImportError:
+    print("pillow yüklencek!")
+    
+
+tke.mainloop()
--- ektra1kodland.py
+++ ektra1kodland.py
@@ -2,16 +2,16 @@ import random
 from colorama import Fore, Style
 import sys
 while True:
-    print("'2*win with *isim yok*' mekanına hoşgeldiniz!")
-    isimce = input("ismin???")
+    print(Fore.LIGHTBLUE_EX + "'2*win with *isim yok*' mekanına hoşgeldiniz!")
+    isimce = input("ismin???"+Style.BRIGHT + Fore.YELLOW)
     if len(isimce) < 3:
         print(Fore.RED+"isminiz çok kısa, lütfen daha uzun bir isim giriniz.")
-        isimce = input("ismin???")
+        isimce = input(Fore.RED+"ismin???")
     if len(isimce) > 10:
         isimce = isimce[:10] + "..."
     any_strings_of_list = ["Mr.Ates","Atesbey","**++py++**",isimce]
     xrt = random.choice(any_strings_of_list)
-    print(xrt, Style.BRIGHT + Fore.LIGHTGREEN_EX +"çıktı kazandın" if xrt == isimce else Style.BRIGHT + Fore.MAGENTA + "çıktı kaybettin" + Style.RESET_ALL)
-    if input("Tekrar oynamak ister misin? (E/H)").upper() != "E":
-        print("Oyun bitti, teşekkürler!")
+    print(Style.BRIGHT + Fore.LIGHTYELLOW_EX + xrt, Style.BRIGHT + Fore.LIGHTGREEN_EX +"çıktı kazandın" if xrt == isimce else Style.BRIGHT + Fore.MAGENTA + "çıktı kaybettin" + Style.RESET_ALL)
+    if input(Fore.CYAN + "Tekrar oynamak ister misin? (E/H)").upper() != "E":
+        print(Fore.CYAN + "Oyun bitti, teşekkürler!")
         sys.exit()
